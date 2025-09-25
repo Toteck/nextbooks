420aa214367f1fd24796fbe2d6e5f6f632edd850
mateus <mateusweslley14@gmail.com>
2025-09-24 21:44:53 -0300

    refactor: enhance LivroCard component for improved performance and maintainability

--- src/components/livros/LivroCard.tsx
+++ src/components/livros/LivroCard.tsx
@@ -16,7 +16,7 @@ import { Dialog, DialogHeader, DialogContent, DialogTitle, DialogTrigger } from 
 
 import Image from "next/image";
 
-import { Star, StarIcon, Trash2, X } from "lucide-react";
+import { Star, Trash2, PenBoxIcon, BookOpen, Calendar } from "lucide-react";
 import { useState } from "react";
 
 interface LivroCardProps {
@@ -61,7 +61,7 @@ const RatingStars = ({ rating }: { rating: number }) => (
 );
 
 export function LivroCard({ livro }: LivroCardProps) {
-  const progress = ((livro.qtdPagesRead || 0) / (livro.pages || 1)) * 100
+  const progress = (((livro.qtdPagesRead || 0) / (livro.pages || 1)) * 100).toFixed(0);
   const [open, setOpen] = useState(false);
 
   return (
@@ -108,15 +108,12 @@ export function LivroCard({ livro }: LivroCardProps) {
               Página {livro.qtdPagesRead} de {livro.pages}
             </span>
             <span className="text-sm text-left text-gray-600 mt-1">
-              {(((livro.qtdPagesRead || 0) / (livro.pages || 1)) * 100).toFixed(
-                0
-              )}
-              %
+              {progress}%
             </span>
           </div>
 
           <div className="h-2 bg-purple-200 rounded-full">
-            <Progress value={progress} className="[&>div]:bg-purple-600" />
+            <Progress value={parseInt(progress)} className="[&>div]:bg-purple-600" />
 
           </div>
         </div>
@@ -188,10 +185,63 @@ export function LivroCard({ livro }: LivroCardProps) {
                 </div>
 
                 <div className="flex items-center gap-1">
-                  <Star width={18} height={18} fill="oklch(85.2% 0.199 91.936)" />
+                  <Star width={18} height={18} fill="oklch(79.5% 0.184 86.047)" stroke="none" />
                   <span className="font-bold">{livro.rating}</span>
                   <span className="text-sm text-gray-500">(127.543 avaliações)</span>
                 </div>
+
+                <div>
+                  <p className="font-medium mb-2">Descrição</p>
+                  <p className="text-sm text-justify text-muted-foreground leading-relaxed">{livro.synopsis}</p>
+                </div>
+
+                {livro.status === StatusLeitura.LENDO && (<div>
+                  <div className="flex items-center justify-between">
+                    <p className="font-medium">Progresso de Leitura</p>
+                    <Button variant={"ghost"} size={"sm"} className="flex items-center gap-1 cursor-pointer hover:bg-gray-200 rounded-md p-1">
+                      <PenBoxIcon className="h-4 w-4" />
+                      Editar
+                    </Button>
+                  </div>
+
+                </div>)}
+
+                {/* Progresso de leitura */}
+
+                <div>
+                  <div className="flex items-center justify-between text-sm mb-2">
+
+                    <span className="flex items-center gap-1">
+                      <BookOpen className="w-4 h-4" />
+                      Página {livro.qtdPagesRead} de {livro.pages}
+                    </span>
+
+                    <span className="text-sm text-left text-gray-600 mt-1">
+                      {progress}
+                      %
+                    </span>
+                  </div>
+
+                  <Progress value={parseInt(progress)} className="[&>div]:bg-purple-600" />
+
+                </div>
+
+                {/* Avaliação e Resenha */}
+                <div className="space-y-2">
+                  <p className="font-medium">Sua avaliação e Resenha</p>
+                  <p className="text-sm text-muted-foreground">Sem Avaliações ainda</p>
+                  <p className="text-sm text-muted-foreground">Sem Reviews ainda</p>
+                </div>
+
+                {/* Data de inicio */}
+                <div className="flex items-center gap-2">
+                  <span className="flex items-center gap-1 text-muted-foreground">
+                    <Calendar className="w-4 h-4 text-muted-foreground" /> Comecei em:
+                  </span>
+                  <span className="">4 de dezembro de 2024</span>
+                </div>
+
+
 
 
 

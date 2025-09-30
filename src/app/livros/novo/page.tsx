98b6a29a75552a86d421c9b694fb75cef95f7450
mateus <mateusweslley14@gmail.com>
2025-09-29 23:38:50 -0300

    refactor: reestrutucte layout

--- src/app/livros/novo/page.tsx
+++ src/app/livros/novo/page.tsx
@@ -1,13 +1,34 @@
+"use client"
 
-import LivroForm from "@/components/livros/LivroForm";
+import { LivroForm } from "@/components/livros/LivroForm";
+import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function NovoLivroPage() {
+
+  const router = useRouter()
+
+  function navigateBack() {
+    router.back()
+    router.refresh()
+  }
+
+
   return (
-    <main>
-      <h1 className="text-4xl font-bold text-center text-zinc-950 mb-8">
-        Adicionar Novo Livro
-      </h1>
+    <div className="pb-4">
+
+      <div className="flex items-center gap-4 mb-4">
+        <ArrowLeft className="w-8 h-8 text-purple-600" onClick={navigateBack} />
+
+        <h1 className="text-2xl font-bold text-center text-purple-600">
+          Adicionar Novo Livro
+        </h1>
+      </div>
+
+
+
       <LivroForm />
-    </main>
-  );
+
+    </div>
+  )
 }

564155a4f9c575e5c9afaaecd54239d519eaf529
mateus <mateusweslley14@gmail.com>
2025-09-30 19:32:23 -0300

    refactor: add form add book component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 // Página Inicial (Dashboard)
 // Mostrar estatísticas(quantos livros, status de leitura, páginas lidas).
-
-import { DefaultButton } from "@/components/livros/Button";
+"use client"
+import { AddBookDialog } from "@/components/AddBookDialog";
+import { DialogDemo } from "@/components/DialogDemo";
+import { DefaultButton } from "@/components/livros/DefaultButton";
 import TabsComponent from "@/components/Tabs";
-import Link from "next/link";
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Plus } from "lucide-react";
 
 // Essa tela é simples, uma visão geral, não detalha livros.
 export default function Home() {
   return (
     <div className="flex flex-col items-center sm:px-8 py-2 w-full">
+
       <main className="flex w-full flex-1 flex-col items-center justify-center gap-6">
-        <Link href={"/livros/novo"}>
-          <DefaultButton label="Adicionar Livro" />
-        </Link>
+
+        <AddBookDialog />
+
         <TabsComponent />
       </main>
     </div>

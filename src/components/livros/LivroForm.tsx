35e71982210d1308f752845081c9fc7fd0339d8a
mateus <mateusweslley14@gmail.com>
2025-09-30 19:27:30 -0300

    refactor: livro form ux

--- src/components/livros/LivroForm.tsx
+++ src/components/livros/LivroForm.tsx
@@ -9,6 +9,8 @@ import { Textarea } from "../ui/textarea"
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup } from "../ui/select"
 import { SelectItem, SelectLabel } from "@radix-ui/react-select"
 import { StatusLeitura } from "@/types/livro"
+import { DefaultButton } from "./DefaultButton"
+import { Link } from "lucide-react"
 
 export function LivroForm() {
 
@@ -17,7 +19,7 @@ export function LivroForm() {
       cover: "",
       title: "",
       author: "",
-      status: "",
+      status: StatusLeitura.LENDO,
       capa: "",
       year: "",
       rating: 0,
@@ -29,61 +31,69 @@ export function LivroForm() {
     }
   })
 
+  const status = form.watch("status")
+
   return (
     <Form {...form}>
       <form className="flex flex-col gap-4">
-        {/* Capa */}
+        <div className="flex flex-row gap-2 w-full">
+
+          {/* Capa */}
+          <FormField
+            control={form.control}
+            name="cover"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-md">Capa do Livro</FormLabel>
+                <FormControl>
+                  <div className="w-[90px] sm:w-[120px] md:w-[150px] aspect-[2/3] bg-gray-100 rounded-lg flex items-center justify-center overflow-hidden border-2 border-dashed border-gray-300 relative">
+                    <Image alt="Capa padrão" src={"/covers/placeholder.png"} width={125}
+                      height={188} sizes="(max-width: 640px) 90px, (max-width: 768px) 120px, 150px" className="w-full h-full object-cover" />
+                  </div>
+
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex flex-col flex-1 items-start gap-4">
+            {/* Campo Título */}
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel className="text-md text-purple-700">Título*</FormLabel>
+                  <FormControl>
+                    <Input label="" placeholder="Digite o título do livro" {...field} className="text-md border border-gray-300 bg-white focus-visible:border-purple-600 focus-visible:ring-2 focus-visible:ring-purple-600 shadow-sm focus-visible:shadow-md transition rounded-md" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* Autor */}
+            <FormField
+              control={form.control}
+              name="author"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel className="text-md text-purple-700">Autor*</FormLabel>
+                  <FormControl>
+                    <Input label="" placeholder="Digite o nome do autor" {...field} className="border border-gray-300 bg-white focus-visible:border-purple-600 focus-visible:ring-2 focus-visible:ring-purple-600 shadow-sm focus-visible:shadow-md transition rounded-md" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
+        {/* CAPA */}
         <FormField
           control={form.control}
           name="cover"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Capa do Livro</FormLabel>
-              <FormControl>
-                <div className="w-full max-w-[250px] aspect-[2/3] bg-gray-100 rounded-lg flex items-center justify-center overflow-hidden border-2 border-dashed border-gray-300">
-                  <Image alt="Capa padrão" src={"/covers/placeholder.png"} width={120} height={180} className="w-full h-full object-cover" />
-                </div>
-
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        {/* Campo Título */}
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel className="">Título*</FormLabel>
-              <FormControl>
-                <Input label="" placeholder="Digite o título do livro" {...field} className="text-md border border-gray-300 bg-white focus-visible:border-purple-600 focus-visible:ring-2 focus-visible:ring-purple-600 shadow-sm focus-visible:shadow-md transition rounded-md" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        {/* Autor */}
-        <FormField
-          control={form.control}
-          name="author"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Autor</FormLabel>
-              <FormControl>
-                <Input label="" placeholder="Digite o nome do autor do livro" {...field} className="border border-gray-300 bg-white focus-visible:border-purple-600 focus-visible:ring-2 focus-visible:ring-purple-600 shadow-sm focus-visible:shadow-md transition rounded-md" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        {/* CAPA */}
-        <FormField
-          control={form.control}
-          name="capa"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>URL da Capa do Livro</FormLabel>
+              <FormLabel className="text-md">URL da Capa do Livro</FormLabel>
               <FormControl>
                 <Input label="" placeholder="Adicione o link da imagem da capa" {...field} className="border border-gray-300 bg-white focus-visible:border-purple-600 focus-visible:ring-2 focus-visible:ring-purple-600 shadow-sm focus-visible:shadow-md transition rounded-md" />
               </FormControl>
@@ -92,13 +102,14 @@ export function LivroForm() {
           )}
         />
 
+
         {/* Status do Livro */}
         <FormField
           control={form.control}
           name="status"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Status do Livro</FormLabel>
+              <FormLabel className="text-md">Adicionar para</FormLabel>
               <FormControl>
                 <Select {...field}>
                   <SelectTrigger className="w-full border border-gray-300 bg-white focus-visible:border-purple-600 focus-visible:ring-2 focus-visible:ring-purple-600 shadow-sm focus-visible:shadow-md transition rounded-md">
@@ -123,27 +134,78 @@ export function LivroForm() {
 
 
 
-        {/* Ano Pulicação */}
-        <FormField
+        <div className="flex items-center gap-2">
+
+
+          {/* Ano Pulicação */}
+          <FormField
+            control={form.control}
+            name="year"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-md">Ano de Publicação</FormLabel>
+                <FormControl>
+                  <Input label="" placeholder="Digite o ano de publicação do livro" {...field} className="border border-gray-300 bg-white focus-visible:border-purple-600 focus-visible:ring-2 focus-visible:ring-purple-600 shadow-sm focus-visible:shadow-md transition rounded-md" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Total de páginas */}
+          <FormField
+            control={form.control}
+            name="pages"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-md">Total de Páginas</FormLabel>
+                <FormControl>
+                  <Input label="" placeholder="Digite o total de páginas do livro" {...field} className="border border-gray-300 bg-white focus-visible:border-purple-600 focus-visible:ring-2 focus-visible:ring-purple-600 shadow-sm focus-visible:shadow-md transition rounded-md" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+        </div>
+        {/* Página Atual */}
+        {status !== StatusLeitura.QUERO_LER && (<FormField
           control={form.control}
-          name="year"
+          name="currentPage"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Ano de Publicação</FormLabel>
+              <FormLabel className="text-md">Página Atual</FormLabel>
               <FormControl>
-                <Input label="" placeholder="Digite o ano de publicação do livro" {...field} className="border border-gray-300 bg-white focus-visible:border-purple-600 focus-visible:ring-2 focus-visible:ring-purple-600 shadow-sm focus-visible:shadow-md transition rounded-md" />
+                <Input label="" placeholder="Digite a quantidade de páginas lidas do livro" {...field} className="border border-gray-300 bg-white focus-visible:border-purple-600 focus-visible:ring-2 focus-visible:ring-purple-600 shadow-sm focus-visible:shadow-md transition rounded-md" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />)}
+
+
+        {/* International Standard Book Number - ISBN */}
+        <FormField
+          control={form.control}
+          name="isbn"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-md">ISBN</FormLabel>
+              <FormControl>
+                <Input label="" placeholder="Digite o ISBN do livro..." {...field} className="border border-gray-300 bg-white focus-visible:border-purple-600 focus-visible:ring-2 focus-visible:ring-purple-600 shadow-sm focus-visible:shadow-md transition rounded-md" />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
+
         {/* Avaliação */}
         <FormField
           control={form.control}
           name="rating"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Sua avaliação</FormLabel>
+              <FormLabel className="text-md">Sua avaliação</FormLabel>
               <FormControl>
                 <RatingStars value={field.value} onChange={field.onChange} />
               </FormControl>
@@ -151,43 +213,14 @@ export function LivroForm() {
             </FormItem>
           )}
         />
-        {/* Total de páginas */}
-        <FormField
-          control={form.control}
-          name="pages"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Total de Páginas</FormLabel>
-              <FormControl>
-                <Input label="" placeholder="Digite o total de páginas do livro" {...field} className="border border-gray-300 bg-white focus-visible:border-purple-600 focus-visible:ring-2 focus-visible:ring-purple-600 shadow-sm focus-visible:shadow-md transition rounded-md" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
 
-
-        {/* Página Atual */}
-        <FormField
-          control={form.control}
-          name="currentPage"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Página Atual</FormLabel>
-              <FormControl>
-                <Input label="" placeholder="Digite a quantidade de páginas lidas do livro" {...field} className="border border-gray-300 bg-white focus-visible:border-purple-600 focus-visible:ring-2 focus-visible:ring-purple-600 shadow-sm focus-visible:shadow-md transition rounded-md" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
         {/* Notas pessoais */}
         <FormField
           control={form.control}
           name="notes"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Notas pessoais</FormLabel>
+              <FormLabel className="text-md">Notas pessoais</FormLabel>
               <FormControl>
                 <Textarea placeholder="Digite algo que queira tomar nota sobre o livro..." {...field} className="border border-gray-300 bg-white focus-visible:border-purple-600 focus-visible:ring-2 focus-visible:ring-purple-600 shadow-sm focus-visible:shadow-md transition rounded-md" />
               </FormControl>
@@ -201,7 +234,7 @@ export function LivroForm() {
           name="synopsis"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Sinopse</FormLabel>
+              <FormLabel className="text-md">Sinopse</FormLabel>
               <FormControl>
                 <Textarea placeholder="Digite a sinopse livro..." {...field} className="border border-gray-300 bg-white focus-visible:border-purple-600 focus-visible:ring-2 focus-visible:ring-purple-600 shadow-sm focus-visible:shadow-md transition rounded-md" />
               </FormControl>
@@ -209,20 +242,8 @@ export function LivroForm() {
             </FormItem>
           )}
         />
-        {/* International Standard Book Number - ISBN */}
-        <FormField
-          control={form.control}
-          name="isbn"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>ISBN</FormLabel>
-              <FormControl>
-                <Input label="" placeholder="Digite o ISBN do livro..." {...field} className="border border-gray-300 bg-white focus-visible:border-purple-600 focus-visible:ring-2 focus-visible:ring-purple-600 shadow-sm focus-visible:shadow-md transition rounded-md" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+
+        <DefaultButton label="Adicionar Livro" />
 
       </form>
     </Form>

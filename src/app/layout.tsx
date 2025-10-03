022e6ab3592b18c869c0437be0567b0c96778f9f
mateus <mateusweslley14@gmail.com>
2025-10-03 16:18:17 -0300

    refactor: accept incoming change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
-<<<<<<< HEAD
-import { Plus, BookOpen, Moon } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { DefaultButton } from "@/components/livros/Button";
-=======
 
 import { Header } from "@/components/Header";
 import { Toaster } from "@/components/ui/sonner";
->>>>>>> main
 
 // Metadados da aplicação
 export const metadata: Metadata = {
@@ -29,14 +23,8 @@ export default function RootLayout({
         <Header />
         <Toaster />
         {/* {Container Global} */}
-<<<<<<< HEAD
-        <main className="container mx-auto px-4 sm:px-6 py-6 pb-[100px] sm:pb-[100px]">
-          {children}
-        </main>
-=======
         <main className="container mx-auto px-4 sm:px-6 py-6">{children}</main>
 
->>>>>>> main
       </body>
     </html>
   );

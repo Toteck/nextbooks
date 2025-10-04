831d8cff9251a8937ce8e0d6cda7f1d90511d5cf
cat.witch9 <ninefisr@gmail.com>
2025-10-04 20:54:06 -0300

    refactor: refactor no livro ts e routes para não interferir em funções já criadas

--- src/data/livros.ts
+++ src/data/livros.ts
@@ -1,9 +1,8 @@
 import { Genre } from "@/types/genre";
 import { Livro, StatusLeitura } from "@/types/livro";
 
-// Dados iniciais (mock)
-
-export let livrosIniciais: Livro[] = [
+// Dados iniciais (imutáveis)
+const livrosIniciais: Livro[] = [
   {
     id: "1",
     title: "Dom Casmurro",
@@ -54,21 +53,24 @@ export let livrosIniciais: Livro[] = [
     status: StatusLeitura.LENDO,
     rating: 4,
     cover: "/covers/revolucao-dos-bichos.jpeg",
-    synopsis: "O livro narra uma história de corrupção e traição e recorre a figuras de animais para retratar as fraquezas humanas e demolir o paraíso comunista proposto pela União Soviética na época de Stalin. A revolta dos animais da quinta contra os humanos é liderada pelos porcos Bola-de-Neve (Snowball) e Napoleão (Napoleon).",
+    synopsis:
+      "O livro narra uma história de corrupção e traição e recorre a figuras de animais para retratar as fraquezas humanas e demolir o paraíso comunista proposto pela União Soviética na época de Stalin. A revolta dos animais da quinta contra os humanos é liderada pelos porcos Bola-de-Neve (Snowball) e Napoleão (Napoleon).",
   },
+];
 
-];
-// mock movido pra cá para poder ser usado na API também
-export const livrosMock: Livro[] = [
-  ...livrosIniciais,
-];
-// função atualizar e deletar livro
+export let livrosDb: Livro[] = [...livrosIniciais];
+
+// Função para obter o estado atual do 'banco de dados'
+export const getLivrosDb = () => livrosDb;
+
+// Função atualizar livro
 export const updateLivro = (livroAtualizado: Livro) => {
-  livrosIniciais = livrosIniciais.map((livro) =>
+  livrosDb = livrosDb.map((livro) =>
     livro.id === livroAtualizado.id ? livroAtualizado : livro
   );
-}
+};
 
+// Função deletar livro
 export const deleteLivro = (id: string) => {
-  livrosIniciais = livrosIniciais.filter((livro) => livro.id !== id);
+  livrosDb = livrosDb.filter((livro) => livro.id !== id);
 };

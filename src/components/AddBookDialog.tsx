667ca7eb02a44b5bf0ba17908050852c29228aaa
mateus <mateusweslley14@gmail.com>
2025-10-04 16:59:09 -0300

    remove tag form and remove comments

--- src/components/AddBookDialog.tsx
+++ src/components/AddBookDialog.tsx
@@ -1,5 +1,4 @@
 "use client"
-import { useState } from "react"
 import {
     Dialog,
     DialogClose,
@@ -10,33 +9,34 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
-import { DefaultButton } from "@/components/livros/DefaultButton"
+
 import { LivroForm } from "@/components/livros/LivroForm"
-import { Plus, Search } from "lucide-react"
+import { Plus } from "lucide-react"
 import { Button } from "./ui/button"
-import { Label } from "./ui/label"
-import { Input } from "./ui/input"
+
 
 export function AddBookDialog() {
-    const [mode, setMode] = useState<"search" | "manual">("search")
 
     return (
         <Dialog>
-            <form>
-                <DialogTrigger asChild>
-                    <Button className="bg-gradient-to-r from-purple-600 to-purple-800 hover:from-purple-500 hover:to-purple-700 text-white px-6 py-3 sm:text-sm md:text-lg font-semibold rounded-lg shadow-md hover:shadow-xl transition-all ring-2 ring-purple-300 hover:scale-105 active:scale-95 hover:cursor-pointer"><Plus className="w-6 h-6 text-white" />Adicionar Livro</Button>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px] max-h-[90vh] overflow-y-auto">
-                    <DialogHeader>
-                        <DialogTitle className="text-purple-700 font-bold">Adicionar Livro</DialogTitle>
-                        <DialogDescription>
-                            Adicione-os manualmente o livro que você deseja.
-                        </DialogDescription>
-                    </DialogHeader>
 
-                    <LivroForm />
-                </DialogContent>
-            </form>
+            <DialogTrigger asChild>
+                <Button className="bg-gradient-to-r from-purple-600 to-purple-800 hover:from-purple-500 hover:to-purple-700 text-white px-6 py-3 sm:text-sm md:text-lg font-semibold rounded-lg shadow-md hover:shadow-xl transition-all ring-2 ring-purple-300 hover:scale-105 active:scale-95 hover:cursor-pointer"><Plus className="w-6 h-6 text-white" />Adicionar Livro</Button>
+
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px] max-h-[90vh] overflow-y-auto">
+                <DialogHeader>
+                    <DialogTitle className="text-purple-700 font-bold">Adicionar Livro</DialogTitle>
+                    <DialogDescription>
+                        Adicione-os manualmente o livro que você deseja.
+                    </DialogDescription>
+                </DialogHeader>
+
+                <LivroForm onSubmit={(values) => {
+                    console.log("Criando livro:", values)
+                }} />
+            </DialogContent>
+
         </Dialog>
     )
 }

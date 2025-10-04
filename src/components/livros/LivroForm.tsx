31084b85a6c252404d5b7c18b5ff94d6b5966505
mateus <mateusweslley14@gmail.com>
2025-10-04 17:06:24 -0300

    refactor: update icons, update default values

--- src/components/livros/LivroForm.tsx
+++ src/components/livros/LivroForm.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { SelectDropdown } from "../SelectDropdown"
@@ -11,7 +10,7 @@ import { Button } from "../ui/button"
 import { Input } from "../ui/input"
 import { toast } from "sonner"
 
-import { Plus, X, RefreshCcw, Library, LibraryBig, LibraryIcon, LibrarySquare, Book } from "lucide-react"
+import { Plus, X, RefreshCcw, Book, Save } from "lucide-react"
 
 import { useForm } from "react-hook-form"
 import Image from "next/image"
@@ -47,24 +46,31 @@ const formSchema = z.object({
   synopsis: z.string().max(2000).optional()
 
 })
-type LivroForm = z.infer<typeof formSchema>
 
-export function LivroForm() {
+export type LivroFormValidation = z.infer<typeof formSchema>
 
-  const form = useForm<z.infer<typeof formSchema>>({
+type LivroFormProps = {
+  livro?: Partial<LivroFormValidation>; // livro existente (para edição)
+  onSubmit: (values: LivroFormValidation) => void; // callback de salvar
+  onCancel?: () => void; // opcional
+}
+
+export function LivroForm({ livro, onSubmit, onCancel }: LivroFormProps) {
+
+  const form = useForm<LivroFormValidation>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      title: "",
-      author: "",
-      cover: "",
-      status: "",
-      year: "",
-      pages: 0,
-      currentPage: 0,
-      isbn: "",
-      rating: 0,
-      notes: "",
-      synopsis: "",
+      title: livro?.title || "",
+      author: livro?.author || "",
+      cover: livro?.cover || "",
+      status: livro?.status || "",
+      year: livro?.year || "",
+      pages: livro?.pages || 0,
+      currentPage: livro?.currentPage || 0,
+      isbn: livro?.isbn || "",
+      rating: livro?.rating || 0,
+      notes: livro?.notes || "",
+      synopsis: livro?.synopsis || "",
     },
   })
 
@@ -75,8 +81,8 @@ export function LivroForm() {
 
   const watchedValues = form.watch();
 
-  const requiredFields: (keyof LivroForm)[] = ["title", "author"];
-  const optionalFields: (keyof LivroForm)[] = status !== "QUERO_LER" ? [
+  const requiredFields: (keyof LivroFormValidation)[] = ["title", "author"];
+  const optionalFields: (keyof LivroFormValidation)[] = status !== "QUERO_LER" ? [
     "status",
     "cover",
     "year",
@@ -110,39 +116,42 @@ export function LivroForm() {
   const progress = Math.round((filledFields / totalFields) * 100);
 
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    try {
-      console.log("Valores do form", values)
-      toast.custom((id) => (
-        <div className="bg-purple-600 text-white p-4 rounded-lg shadow-lg">
-          <span className="flex flex-row items-center justify-center gap-4"><Book /> Livro criado com sucesso!</span>
-        </div>
-      ), {
-        description: `O livro "${values.title}" foi salvo.`,
-        position: "top-center",
-        duration: 5000
-      })
+  function handleFormSubmit(values: z.infer<typeof formSchema>) {
+    onSubmit(values)
 
-    } catch (error) {
-      toast.custom((id) => (
-        <div className="bg-red-600 text-white p-4 rounded-lg shadow-lg">
-          <span className="flex flex-row items-center justify-center gap-4"><X /> Erro ao criar livro. Tente novamente!</span>
-        </div>
-      ), {
-        description: `O livro "${values.title}" foi salvo.`,
-        position: "top-center",
-        duration: 5000
-      })
-    }
+    // try {
+    //   console.log("Valores do form", values)
+    //   toast.custom((id) => (
+    //     <div className="bg-purple-600 text-white p-4 rounded-lg shadow-lg">
+    //       <span className="flex flex-row items-center justify-center gap-4"><Book /> Livro criado com sucesso!</span>
+    //     </div>
+    //   ), {
+    //     description: `O livro "${values.title}" foi salvo.`,
+    //     position: "top-center",
+    //     duration: 5000
+    //   })
+
+    // } catch (error) {
+    //   toast.custom((id) => (
+    //     <div className="bg-red-600 text-white p-4 rounded-lg shadow-lg">
+    //       <span className="flex flex-row items-center justify-center gap-4"><X /> Erro ao criar livro. Tente novamente!</span>
+    //     </div>
+    //   ), {
+    //     description: `O livro "${values.title}" foi salvo.`,
+    //     position: "top-center",
+    //     duration: 5000
+    //   })
+    // }
   }
 
-  function onReset() {
+  function handleReset() {
     form.reset()
+    onCancel?.();
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit(handleFormSubmit)} className="flex flex-col gap-4">
 
         <div className="space-y-2">
           <p className="text-sm text-purple-700 font-bold">Progresso do preenchimento: {progress}%</p>
@@ -350,10 +359,11 @@ export function LivroForm() {
           )}
         />
 
-        <Button type="submit" variant={"outline"} className="text-white bg-gradient-to-r from-purple-600 to-purple-800 hover:from-purple-500 hover:to-purple-700 px-6 py-3 font-bold rounded-lg shadow-md hover:shadow-xl transition-all ring-2 ring-purple-300 hover:scale-105 active:scale-95 hover:cursor-pointer"><Plus className="size-4" />Salvar</Button>
+        <Button type="submit" variant={"outline"} className="text-white bg-gradient-to-r from-purple-600 to-purple-800 hover:from-purple-500 hover:to-purple-700 px-6 py-3 font-bold rounded-lg shadow-md hover:shadow-xl transition-all ring-2 ring-purple-300 hover:scale-105 active:scale-95 hover:cursor-pointer">{livro ? (<span className="flex items-center gap-2"><Plus className="size-4" />Atualizar Livro</span>) : <span className="flex items-center gap-2"><Save className="size-4" />Salvar Livro</span>}</Button>
 
         <DialogClose asChild>
-          <Button variant={"outline"} onClick={onReset} className=" text-purple-700 px-6 py-3 sm:text-sm md:text-lg font-bold rounded-lg shadow-md hover:shadow-xl transition-all ring-2 ring-purple-300 hover:scale-105 active:scale-95 hover:cursor-pointer"><X className="text-purple-700 font-bold size-4" />Cancelar</Button>
+          <Button variant={"outline"} onClick={handleReset} className=" text-purple-700 px-6 py-3 sm:text-sm md:text-lg font-bold rounded-lg shadow-md hover:shadow-xl transition-all ring-2 ring-purple-300 hover:scale-105 active:scale-95 hover:cursor-pointer">
+            <X className="text-purple-700 font-bold size-4" />Cancelar</Button>
         </DialogClose>
 
 

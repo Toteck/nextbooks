5edea19c4ac558f3d094a93fa01443550a3a4308
mateus <mateusweslley14@gmail.com>
2025-10-05 16:45:44 -0300

    refactor: remove 4 span, add default button, add control FormBookDialog

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,37 +5,45 @@ import { BookOpen, Plus, Moon, Sun, UserCircle2Icon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 
-import { AddBookDialog } from "./AddBookDialog";
+import { FormBookDialog } from "./FormBookDialog";
 import { useState } from "react";
+import { DefaultButton } from "./DefaultButton";
 
 export function Header() {
-    const [dark, setDark] = useState(false);
+  const [dark, setDark] = useState(false);
+  const [isFormBookDialogOpen, setIsFormBookDialogOpen] = useState(false)
 
-    return (
-      <header className="flex justify-between items-center p-4 border-b bg-purple-200">
-        <div className="flex items-center gap-2">
-          <BookOpen className="h-6 w-6 text-purple-600" />{" "}
-          <span className="text-xl font-semibold">NextBooks</span>
+  return (
+    <>
+      <header className="border-b bg-purple-50">
+        <div className="container mx-auto px-4 sm:px-6 flex items-center justify-between p-4">
+          <div className="flex items-center gap-2">
+            <BookOpen className="h-6 w-6 text-purple-600" />{" "}
+            <span className="text-xl font-semibold">NextBooks</span>
+          </div>
+          <div className="flex items-center gap-4">
+            <Button
+              variant="ghost"
+              size="icon"
+              className="houver:bg-purple-500 hover:cursor-pointer"
+              onClick={() => setDark(!dark)}
+            >
+              {dark ? (
+                <Sun className="h-6 w-6 text-purple-700" />
+              ) : (
+                <Moon className="h-4 w-4 text-purple-500" />
+              )}
+            </Button>
+            <DefaultButton icon={Plus} variant="outline" label="Adicionar Livro" responsive onClick={() => setIsFormBookDialogOpen(true)} />
+
+          </div>
         </div>
-        <div className="flex items-center gap-4">
-          <Button
-            variant="ghost"
-            size="icon"
-            className="houver:bg-purple-500 hover:cursor-pointer"
-            onClick={() => setDark(!dark)}
-          >
-            {dark ? (
-              <Sun className="h-6 w-6 text-purple-700" />
-            ) : (
-              <Moon className="h-4 w-4 text-purple-500" />
-            )}
-          </Button>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <AddBookDialog />
-        </div>
+
+
       </header>
-    );
+      <FormBookDialog
+        isOpen={isFormBookDialogOpen}
+        onClose={() => setIsFormBookDialogOpen(false)} isEditing />
+    </>
+  );
 }
